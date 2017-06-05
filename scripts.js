fc709b196116b7d3e9613af307e841b3b036924b
katarzynakich <katarzynakich@gmail.com>
2017-06-05 21:16:25 +0200

    usuniety =

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 var buttons = document.getElementsByClassName('button');
 alert('Liczba przycisków: ' + buttons.length);
 
-for (var x = 0; x <= buttons.length; x++) {
+for (var x = 0; x < buttons.length; x++) {
   var text = buttons[x].innerText;
   alert("Tekst: " + text);
 }
