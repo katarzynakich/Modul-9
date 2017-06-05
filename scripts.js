bc9edb4b6ce3cb59511c8f0b7a4058ff314db16e
katarzynakich <katarzynakich@gmail.com>
2017-06-06 00:54:32 +0200

    jest choinka

--- scripts.js
+++ scripts.js
@@ -1,14 +1,17 @@
 function drawTree(n) {
-  var star;
+  var star, space;
 
   for (var i = 1; i <= n ; i++) {
     star = "";
-    for (var j = 1; j <= i; j++) {
+    space = " ";
+    for (var k = n-1; k >= i; k--) {
+      space += ' ';
+    }
+    for (var j = 1; j <= (2 * i) - 1; j++) {
       star += '*';
     }
-    console.log(star);
+    console.log(space, star);
   }
-  
-}
 
-drawTree(5);
+}
+drawTree(7);
