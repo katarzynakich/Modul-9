e93ffd46d29a572d81961bde32b4f3174c982e6a
katarzynakich <katarzynakich@gmail.com>
2017-06-05 15:18:29 +0200

    9-5

---
+++ scripts.js
@@ -0,0 +1,7 @@
+var buttons = document.getElementsByClassName('button');
+alert('Liczba przycisków: ' + buttons.length);
+
+for (var x = 0; x <= buttons.length; x++) {
+  var text = buttons[x].innerText;
+  alert("Tekst: " + text);
+}
