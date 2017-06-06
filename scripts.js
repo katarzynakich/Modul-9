d4d2a776cbacf91c186a8f0506516f772010f908
katarzynakich <katarzynakich@gmail.com>
2017-06-06 19:57:41 +0200

    poprawione

--- scripts.js
+++ scripts.js
@@ -1,3 +1,7 @@
+var triangle1Area = getTriangleArea(10, 15);
+var triangle2Area = getTriangleArea(-2, 1500100900);
+var triangle3Area = getTriangleArea(777, 333);
+
 function getTriangleArea(a, h) {
   if(a > 0 && h > 0) {
    return a*h/2;
@@ -6,3 +10,4 @@ function getTriangleArea(a, h) {
    return 'Wrong numbers';
   }
 }
+console.log(triangle1Area, triangle2Area, triangle3Area);
